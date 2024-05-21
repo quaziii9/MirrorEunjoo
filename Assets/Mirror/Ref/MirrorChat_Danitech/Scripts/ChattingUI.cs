e09db7fe44347a071dfe7e9716b5286be4939021
조현승 <danitech171791@gmail.com>
2024-05-21 13:48:05 +0900

    [feat] 채팅 - 채팅 UI NetworkBehaviour 상속받도록 변경
    - NetworkBehaviour의 OnStartServer, OnStartClient 오버라이딩

--- Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/ChattingUI.cs
+++ Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/ChattingUI.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
 
-public class ChattingUI : MonoBehaviour
+public class ChattingUI : NetworkBehaviour
 {
     [Header("UI")]
     [SerializeField] Text Text_ChatHistory;
     [SerializeField] Scrollbar ScrollBar_Chat;
     [SerializeField] InputField Input_ChatMsg;
     [SerializeField] Button Btn_Send;
+
+    public override void OnStartServer()
+    {
+    }
+
+    public override void OnStartClient()
+    {
+    }
 }

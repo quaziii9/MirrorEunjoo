ab7e8e281ff603d7c0681f95992ad3e50899701f
조현승 <danitech171791@gmail.com>
2024-05-21 10:26:45 +0900

    [feat] 채팅 - 로그인 팝업 기초 구조 1
    - UI에서 사용할 오브젝트 선언

---
+++ Assets/Mirror/Ref/MirrorChat_Danitech/Scenes/LoginPopup.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginPopup : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] internal InputField Input_NetworkAdress;
+    [SerializeField] internal InputField Input_UserName;
+
+    [SerializeField] internal Button Btn_StartAsHostServer;
+    [SerializeField] internal Button Btn_StartAsClient;
+
+    [SerializeField] internal Text Text_Error;
+}

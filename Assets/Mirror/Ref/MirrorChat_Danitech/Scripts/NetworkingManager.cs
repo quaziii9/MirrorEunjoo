5206a7cb47d9a9ddc19c0894f104a739dee39e78
조현승 <danitech171791@gmail.com>
2024-05-22 11:37:07 +0900

    [feat] 채팅 - 네트워크 매니저 기초구조 2
    - NetworkManager의 함수들 오버라이딩
    - 그 외 필요 함수 선언 (OnInputValueChanged_SetHostName)

--- Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/NetworkingManager.cs
+++ Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/NetworkingManager.cs
@@ -3,4 +3,17 @@ using Mirror;
 
 public class NetworkingManager : NetworkManager
 {
+    public void OnInputValueChanged_SetHostName(string hostName)
+    {
+    }
+
+    public override void OnServerDisconnect(NetworkConnectionToClient conn)
+    {
+        base.OnServerDisconnect(conn);
+    }
+
+    public override void OnClientDisconnect()
+    {
+        base.OnClientDisconnect();
+    }
 }

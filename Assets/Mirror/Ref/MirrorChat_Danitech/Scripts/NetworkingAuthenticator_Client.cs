b63d8642c42c2849eb54dc1620ddb1ef3ffecb54
조현승 <danitech171791@gmail.com>
2024-05-22 14:54:18 +0900

    [feat] 채팅 - 네트워크 인증자(클라) / 연관 함수 추가

--- Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/NetworkingAuthenticator_Client.cs
+++ Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/NetworkingAuthenticator_Client.cs
@@ -5,5 +5,26 @@ using Mirror;
 
 public partial class NetworkingAuthenticator
 {
-    
+    public void SetPlayerName(string username)
+    {
+    }
+
+    public override void OnStartClient()
+    {
+    }
+
+    public override void OnStopClient()
+    {
+    }
+
+    // 클라에서 인증 요청 시 불려짐
+    public override void OnClientAuthenticate()
+    {
+    }
+
+    public void OnAuthResponseMessage(AuthResMsg msg)
+    {
+
+    }
+
 }

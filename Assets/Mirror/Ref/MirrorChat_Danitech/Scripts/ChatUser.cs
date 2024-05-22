09bb68fbd469fabd4efa8148dcf67e4b8fdb37e5
조현승 <danitech171791@gmail.com>
2024-05-22 15:51:15 +0900

    [feat] 채팅 - 유저 / 동기화되어야 되는 변수 추가
    - syncVar 어트리뷰트를 통해 playerName 변수를 모든 클라에 자동 동기화

--- Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/ChatUser.cs
+++ Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/ChatUser.cs
@@ -3,4 +3,9 @@ using Mirror;
 
 public class ChatUser : NetworkBehaviour
 {
+    // SyncVar - 서버 변수를 모든 클라에 자동 동기화하는데 사용됨
+    // 클라가 직접 변경하면 안되고, 서버에서 변경해야 함
+    [SyncVar]
+    public string _playerName;
+
 }

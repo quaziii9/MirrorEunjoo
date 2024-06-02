40680b8ab83964f88efd9c3a5d555beec6749cc3
eunjoo <quaziii9gamedev@gmail.com>
2024-06-02 21:54:57 +0900

    [feat] 채팅 - 로그인 팝업 유저이름 변경 시 처리

--- Assets/Mirror/Ref/MirrorChat_Danitech/Scenes/LoginPopup.cs
+++ Assets/Mirror/Ref/MirrorChat_Danitech/Scenes/LoginPopup.cs
@@ -27,6 +27,16 @@ public class LoginPopup : MonoBehaviour
         SetDefaultNetworkAddress();
     }
 
+    private void OnEnable()
+    {
+        Input_UserName.onValueChanged.AddListener(OnValueChanged_ToggleButton);
+    }
+
+    private void OnDisable()
+    {
+        Input_UserName.onValueChanged.RemoveListener(OnValueChanged_ToggleButton);
+    }
+
     private void SetDefaultNetworkAddress()
     {
         // 네트워크 주소 없는 경우, 디폴트 세팅
@@ -37,5 +47,25 @@ public class LoginPopup : MonoBehaviour
 
         // 네트워크 주소 공란으로 변경될 경우를 대비해 기존 네트워크 주소 보관
         _originNetworkAddress = NetworkManager.singleton.networkAddress;
+    }
+
+    private void CheckNatworkAddressValidOnUpdate()
+    {
+        if(string.IsNullOrWhiteSpace(NetworkManager.singleton.networkAddress))
+        {
+            NetworkManager.singleton.networkAddress = _originNetworkAddress;
+        }
+
+        if(Input_NetworkAdress.text != NetworkManager.singleton.networkAddress)
+        {
+            Input_NetworkAdress.text = NetworkManager.singleton.networkAddress;
+        }
+    }
+
+    public void OnValueChanged_ToggleButton(string userName)
+    {
+        bool isUserNameValid = !string.IsNullOrWhiteSpace(userName);
+        Btn_StartAsHostServer.interactable = isUserNameValid;
+        Btn_StartAsClient.interactable = isUserNameValid;
     }
 }

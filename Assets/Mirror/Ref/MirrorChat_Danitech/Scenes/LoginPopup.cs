47c5b7b4f61d4352355b4481635f190beed8dccc
eunjoo <quaziii9gamedev@gmail.com>
2024-06-02 22:21:38 +0900

    [feat] 채팅 - 로그인 팝업 설명 주석처리

--- Assets/Mirror/Ref/MirrorChat_Danitech/Scenes/LoginPopup.cs
+++ Assets/Mirror/Ref/MirrorChat_Danitech/Scenes/LoginPopup.cs
@@ -4,6 +4,9 @@ using Mirror;
 
 public class LoginPopup : MonoBehaviour
 {
+    // internal
+    // 동일 어셈블리(프로젝트) 내에서만 접근가능
+
     [Header("UI")]
     [SerializeField] internal InputField Input_NetworkAdress;
     [SerializeField] internal InputField Input_UserName;
@@ -13,34 +16,48 @@ public class LoginPopup : MonoBehaviour
 
     [SerializeField] internal Text Text_Error;
 
+    // LoginPopup 클래스의 정적 인스턴스 변수(단일 인스턴스를 저장)
+    // private set을 사용하여 외부에서 인스턴스 변경할수 없음
     public static LoginPopup instance { get; private set; }
 
+    // 네트워크 주소의 기본값 저장 
     private string _originNetworkAddress;
 
     private void Awake()
     {
+        // instance를 현재 객체로 설정하여 싱글톤 인스턴스 초기화,
+        // Loginpopup 클래스의 인스턴스가 하나만 존재하도록 보장
         instance = this;
     }
 
     private void Start()
     {
+        // 기본 네트워크 주소를 설정하는 메서드 호출
         SetDefaultNetworkAddress();
     }
 
     private void OnEnable()
     {
+        // 사용자 이름 입력 필드의 값 변경 이벤트 리스너 등록
         Input_UserName.onValueChanged.AddListener(OnValueChanged_ToggleButton);
     }
 
     private void OnDisable()
     {
+        // 사용자 이름 입력 필드의 값 변경 이벤트 리스너 해제
         Input_UserName.onValueChanged.RemoveListener(OnValueChanged_ToggleButton);
     }
 
     private void SetDefaultNetworkAddress()
     {
         // 네트워크 주소 없는 경우, 디폴트 세팅
-        if(string.IsNullOrWhiteSpace(NetworkManager.singleton.networkAddress))
+        // IsNullOrWhiteSpace : 문자열이 'null', 빈문자열, 또는 공백문자로만 이루어져 있는지 확인
+
+        // 예제
+        // string input = "   ";
+        // bool isNullOrWhiteSpace = string.IsNullOrWhiteSpace(input);
+        // isNullOrWhiteSpace는 true가 됨
+        if (string.IsNullOrWhiteSpace(NetworkManager.singleton.networkAddress))
         {
             NetworkManager.singleton.networkAddress = "localhost";
         }
@@ -49,13 +66,16 @@ public class LoginPopup : MonoBehaviour
         _originNetworkAddress = NetworkManager.singleton.networkAddress;
     }
 
+
     private void CheckNatworkAddressValidOnUpdate()
     {
+        // 네트워크 주소가 유효하지않으면 _originNetworkAddress로 되돌림
         if(string.IsNullOrWhiteSpace(NetworkManager.singleton.networkAddress))
         {
             NetworkManager.singleton.networkAddress = _originNetworkAddress;
         }
 
+        // 입력된 네트워크 주소가 현재 네트워크 매니저의 주소와 일치하지 않을시 동기화
         if(Input_NetworkAdress.text != NetworkManager.singleton.networkAddress)
         {
             Input_NetworkAdress.text = NetworkManager.singleton.networkAddress;
@@ -64,6 +84,7 @@ public class LoginPopup : MonoBehaviour
 
     public void OnValueChanged_ToggleButton(string userName)
     {
+        //  값이 변경될때 호출, 비어있지 않은 경우 버튼을 활성화
         bool isUserNameValid = !string.IsNullOrWhiteSpace(userName);
         Btn_StartAsHostServer.interactable = isUserNameValid;
         Btn_StartAsClient.interactable = isUserNameValid;

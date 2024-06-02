b5b0597e3569cec29184e873527ab579b353c5a5
eunjoo <quaziii9gamedev@gmail.com>
2024-06-02 22:53:37 +0900

    [init] 채팅 - 채팅 UI.cs 추가

---
+++ Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/ChattingUI.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChattingUI : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] Text Text_ChatHistory;
+    [SerializeField] Scrollbar ScrollBar_Chat;
+    [SerializeField] InputField Input_ChatMsg;
+    [SerializeField] Button Btn_Send;
+}

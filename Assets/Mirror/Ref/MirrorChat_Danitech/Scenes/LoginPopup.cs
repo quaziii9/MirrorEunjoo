3eb4dbe4a72aa0a25ea72dfd81137e49d6e7af10
eunjoo <quaziii9gamedev@gmail.com>
2024-06-02 21:31:00 +0900

    [feat] 채팅 - 로그인 팝업 네트워크 주소 변경 감지 처리 추가
    - Update() 추가
    - Update에 CheckNetworkAddressValidOnUpdate() 추가

--- Assets/Mirror/Ref/MirrorChat_Danitech/Scenes/LoginPopup.cs
+++ Assets/Mirror/Ref/MirrorChat_Danitech/Scenes/LoginPopup.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
 
 public class LoginPopup : MonoBehaviour
 {
@@ -14,8 +15,27 @@ public class LoginPopup : MonoBehaviour
 
     public static LoginPopup instance { get; private set; }
 
+    private string _originNetworkAddress;
+
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        SetDefaultNetworkAddress();
+    }
+
+    private void SetDefaultNetworkAddress()
+    {
+        // 네트워크 주소 없는 경우, 디폴트 세팅
+        if(string.IsNullOrWhiteSpace(NetworkManager.singleton.networkAddress))
+        {
+            NetworkManager.singleton.networkAddress = "localhost";
+        }
+
+        // 네트워크 주소 공란으로 변경될 경우를 대비해 기존 네트워크 주소 보관
+        _originNetworkAddress = NetworkManager.singleton.networkAddress;
     }
 }

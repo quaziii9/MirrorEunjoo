317ee0d372b5403a51fd8e284b9a125d0b96a36f
eunjoo <quaziii9gamedev@gmail.com>
2024-06-02 22:58:04 +0900

    [feat] 채팅 - 채팅 UI 연결된 플레이어 정보를 관리할 컨테이너 추가
    [feat] 채팅 - 채팅 UI 클라 시작 시 채팅목록 내용 초기화

--- Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/ChattingUI.cs
+++ Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/ChattingUI.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
+using System.Collections.Generic;
 
-public class ChattingUI : MonoBehaviour
+public class ChattingUI : NetworkBehaviour
 {
     [Header("UI")]
     [SerializeField] Text Text_ChatHistory;
     [SerializeField] Scrollbar ScrollBar_Chat;
     [SerializeField] InputField Input_ChatMsg;
     [SerializeField] Button Btn_Send;
+
+    // 서버 온리 - 연결된 플레이어들 이름
+    internal static readonly Dictionary<NetworkConnectionToClient, string> _connectedNameDic = new Dictionary<NetworkConnectionToClient, string>();
+    public override void OnStartServer()
+    {
+        _connectedNameDic.Clear();
+    }
+
+    public override void OnStartClient()
+    {
+        Text_ChatHistory.text = string.Empty;
+    }
 }
